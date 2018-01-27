187506f6acb2519bbba5a1ecc8149b9724164243
JTorrus <jtorrusgalindo@gmail.com>
2018-01-27 23:44:37 +0100

    Routes to components added

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,35 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Dashboard from '@/components/Dashboard'
+import NewEvent from '@/components/NewEvent'
+import EditEvent from '@/components/EditEvent'
+import ViewEvent from '@/components/ViewEvent'
 
 Vue.use(Router)
-
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'dashboard',
+      component: Dashboard
+    },
+
+    {
+      path: '/new',
+      name: 'new-event',
+      component: NewEvent
+    },
+
+    {
+      path: '/view/:event_id',
+      name: 'view-event',
+      component: ViewEvent
+    },
+
+    {
+      path: '/edit/:event_id',
+      name: 'edit-event',
+      component: EditEvent
     }
   ]
 })
