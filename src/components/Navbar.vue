30e546cb8e80a9f2fd2344a47fe5e92f83ff869d
JTorrus <jtorrusgalindo@gmail.com>
2018-01-28 00:00:16 +0100

    Floating button and navbar added to page

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,5 +1,11 @@
 <template>
-  <div id="navbar">
-    Navbar
-  </div>
+  <nav>
+    <div class="nav-wrapper pink">
+      <div class="container">
+        <router-link to="/" class="brand-logo">
+          Event Manager
+        </router-link>
+      </div>
+    </div>
+  </nav>
 </template>
