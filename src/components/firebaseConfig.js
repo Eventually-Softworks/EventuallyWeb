a3ca65759369d4838a1b4aa5e7a517232fce3a59
JTorrus <jtorrusgalindo@gmail.com>
2018-01-27 23:25:14 +0100

    init project with Firebase+Firestore implementation

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+  apiKey: "AIzaSyAjclGnOgWmOGJY8z20QNvNRly5Q7VbFHI",
+  authDomain: "evedb-98c72.firebaseapp.com",
+  databaseURL: "https://evedb-98c72.firebaseio.com",
+  projectId: "evedb-98c72",
+  storageBucket: "evedb-98c72.appspot.com",
+  messagingSenderId: "928318478685"
+}
