26b7658a5939df5a8b7f00ada323c7d07c301a16
JTorrus <jtorrusgalindo@gmail.com>
2018-02-03 14:44:10 +0100

    First tests with Vuetify

--- src/components/NewEvent.vue
+++ src/components/NewEvent.vue
@@ -1,4 +1,4 @@
-<template>
+<!--<template>
   <div id="new-event">
     <h3>New Event</h3>
     <div class="row">
@@ -36,6 +36,44 @@
       </form>
     </div>
   </div>
+</template>-->
+
+<template>
+  <v-form v-model="valid" ref="form" lazy-validation>
+    <v-text-field
+    label="Nombre del evento"
+    v-model="name"
+    :rules="nameRules"
+    :counter="10"
+    required>
+    </v-text-field>
+
+    <v-select
+    label="Categoría del evento"
+    v-model="category"
+    :items="categories"
+    :rules="[v => !!v || 'Selecciona una categoría']"
+    required>
+    </v-select>
+
+    <v-menu
+      lazy
+      :close-on-content-click="false"
+      v-model="menu"
+      transition="scale-transition"
+      offset-y
+      full-width
+      :nudge-right="40"
+      max-width="290px"
+      min-width="290px">
+      <v-text-field
+        slot="activator"
+        label="Picker in menu"
+        v-model="date"
+        prepend-icon="event"
+        readonly>
+      </v-text-field>
+  </v-form>
 </template>
 
 <script>
